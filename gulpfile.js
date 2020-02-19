48d5a5c9cc1eddd27926675b17aa0dbdad405f42
Rafal Bartkowski <scorpionikr@gmail.com>
2020-02-19 20:04:01 +0100

    Layout

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const sourcemaps = require('gulp-sourcemaps');
+
+gulp.task("sass", function() {
+    return gulp.src('scss/main.scss')
+        .pipe(sourcemaps.init())
+        //mapa wyzej
+        .pipe(sass({errLogToConsole: true, outputStyle: 'expanded'}))
+        .pipe(sourcemaps.write())
+        //mapa wyzej
+        .pipe(gulp.dest('css'))
+});
+
+gulp.task("watch", function() {
+    gulp.watch("scss/**/*.scss", gulp.series("sass"))
+//    wszystkie pliki w podkatalogu scss
+});
