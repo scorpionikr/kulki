15b4b464cabc9e8d60a3fe8a709dd864ce591db6
Rafal Bartkowski <scorpionikr@gmail.com>
2020-03-03 21:16:08 +0100

    Poprawiona funkcja kasowania kulek i punkty

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 document.addEventListener("DOMContentLoaded", function(){
     const table = [];
     const tableofcolors = ["yellow", "red", "green", "blue", "orange"];
@@ -26,84 +25,115 @@ document.addEventListener("DOMContentLoaded", function(){
     }
 
     function nextturn (array) {
+        checkfive(balls);
         for (let i=0; i< 3; i++ ) {
             let ball = randomballs(array);
-            checkfive(balls);
             if (ball.className != "visible") {
                 ball.classList.toggle("visible");
                 ball.style.backgroundColor = colorrandom(tableofcolors);
-                // checkfive(balls);
-            } else {
-                ball = randomballs(array);
             }
         }
     }
 
     function selectball (array, array2) {
-
         array.forEach(function (kulka) {
             kulka.addEventListener("click", function () {
-                // console.log(this)
                 if (this.className == "visible") {
                     argument = this.getAttribute("style", "backgroundColor")
-                    // let log = true;
                 }
                 return argument;
             });
         })
-
         array2.forEach(function(empt) {
             empt.addEventListener("click", function () {
-                // console.log(funtion(kulka))
                     empt.firstChild.classList.toggle("visible");
-                    // console.log(argument);
                     empt.firstChild.setAttribute("style", argument);
-                    // checkfive(balls);
             });
-
         })
     }
 
-    function checkfive (array) {
+    for (let i=0; i< 9; i++) {
+        table.push (rows[i].querySelectorAll("td"));
+    }
+    // console.log(table[1][1].querySelector("span"))
+    // console.log(table.length);
+
+
+    function checkrows (array) {
         let tablefrom = [];
         let argument1;
         let argument2;
-        let counter =1;
-        for (let i = 0; i < array.length - 1; i++) {
-                argument1 = array[i].getAttribute("style", "backgroundColor")
-                argument2 = array[i + 1].getAttribute("style", "backgroundColor")
-                if (argument1 == argument2 && argument1 != null && argument2 != null) {
-                        tablefrom.push(i);
-                        tablefrom.push(i+1);
-                        counter = counter +1;
-                        console.log(counter)
-                        console.log(tablefrom)
-                        // console.log(tablefrom.length)
+        let counter = 1;
+        for (let i = 0; i < array.length; i++) {
+            for (let k = 0; k < array[i].length; k++) {
+            argument1 = array[i][k].querySelector("span").getAttribute("style", "backgroundColor")
+            argument2 = array[i][k + 1].querySelector("span").getAttribute("style", "backgroundColor")
+            if (argument1 == argument2 && argument1 != null && argument2 != null) {
+                tablefrom.push([i][k]);
+                // tablefrom.push([i][k+1]);
+                counter = counter +1;
+                console.log(counter)
+                console.log(tablefrom)
                 }
+            }
         }
-                if (counter % 5 == 0) {
-                    for (let j = 0; j < tablefrom.length; j++) {
-                        array[tablefrom[j]].classList.remove("visible");
-                        }
-                    tablefrom.length = 0;
-                    counter = 1;
-                    navigame[1].firstElementChild.innerText = parseInt(navigame[1].firstElementChild.innerText) + 2;
-                }
+        // if (counter % 5 == 0) {
+        //     for (let j = 0; j < tablefrom.length; j++) {
+        //         array[tablefrom[j]].querySelector("span").classList.remove("visible");
+        //         array[tablefrom[j]].querySelector("span").style.backgroundColor = "";
+        //     }
+        //     counter = 1;
+        //     tablefrom.length = 0;
+        //     navigame[1].firstElementChild.innerText = parseInt(navigame[1].firstElementChild.innerText) + 5;
+        // }
+
+    }
+
+
+    function checkfive (array) {
+        let tablefrom = [];
+        let tabledest = [];
+        let argument1;
+        let argument2;
+        let counter = 1;
+        for (let i = 0; i < array.length - 1; i++) {
+            argument1 = array[i].getAttribute("style", "backgroundColor")
+            argument2 = array[i + 1].getAttribute("style", "backgroundColor")
+            if (argument1 == argument2 && argument1 != null && argument2 != null) {
+                tablefrom.push(i);
+                tablefrom.push(i+1);
+                counter = counter +1;
+                tabledest = tablefrom.filter(function(element, index, array) {
+                    return (i+1) - i ==1;
+                });
+                console.log(counter)
+                console.log(tabledest)
+            }
+        }
+        if (counter % 5 == 0) {
+            for (let j = 0; j < tabledest.length; j++) {
+                array[tabledest[j]].classList.remove("visible");
+                array[tabledest[j]].style.backgroundColor = "";
+            }
+            counter = 1;
+            tablefrom.length = 0;
+            tabledest.length = 0;
+            navigame[1].firstElementChild.innerText = parseInt(navigame[1].firstElementChild.innerText) + 5;
+        }
     }
 
     function checkfull (array) {
-        let counter = 0;
+        let counter1 = 0;
         array.forEach(function (kulka) {
             if (kulka.className == "visible") {
-                counter = counter +1;
+                counter1 = counter1 +1;
             }
-            });
-        return counter;
+        });
+        return counter1;
     }
 
     function cleargame (array) {
         array.forEach(function(kulka) {
-                // console.log(this)
                 kulka.classList.remove("visible");
             });
     }
@@ -113,7 +143,6 @@ document.addEventListener("DOMContentLoaded", function(){
             const interval = setInterval(function(){
                 if (checkfull(array) < 100) {
                     nextturn(balls);
-                    // checkfive(balls);
                 } else {
                     clearInterval(interval);
                     end_game();
@@ -131,7 +160,6 @@ document.addEventListener("DOMContentLoaded", function(){
     }
 
     function newgame(button) {
-
         button.addEventListener("click", function () {
             form.addEventListener("submit", function(event){
             event.preventDefault();
@@ -146,11 +174,10 @@ document.addEventListener("DOMContentLoaded", function(){
                 alert.innerText ="";
                 cleargame(balls);
                 nextturn(balls);
-                selectball(balls, empty);
                 nextballs (balls);
+                selectball(balls, empty);
 
             } else {
-                // console.log(alert)
                 alert.innerText = "Name is to short!";
             }
         });
@@ -174,7 +201,6 @@ document.addEventListener("DOMContentLoaded", function(){
         game.classList.toggle("unvisible");
         end.classList.toggle("unvisible");
         navigame[0].parentElement.parentElement.classList.toggle("unvisible");
-        // navi[0].parentElement.parentElement.classList.toggle("unvisible");
         const interval = setInterval(function(){
             location.reload();
             clearInterval(interval);
